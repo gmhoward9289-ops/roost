1ffd16e45663b2c3a186d864105fb1ae0c26b246
George <dev@swamplink.com>
2026-08-20 07:22:15 -0400

    chore(main): release 0.9.0

--- roost.py
+++ roost.py
@@ -77,7 +77,7 @@ from urllib.parse import unquote, urlparse
 # greedy `sed -n 's/^__version__ = "\(.*\)"/\1/p'`, which would swallow a
 # trailing comment into the version string.
 # x-release-please-start-version
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 # x-release-please-end
 
 SCHEMA_SNAPSHOT = "roost.snapshot.v1"
